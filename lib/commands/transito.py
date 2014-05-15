f2aecf1c87cab874ae7b5e2a4543ce8e91c19930
tiagodexter <tiagodexter@gmail.com>
2014-05-15 11:56:06 -0300

    Revert Transit

--- lib/commands/transito.py
+++ lib/commands/transito.py
@@ -7,29 +7,14 @@ class Transito(Base_Command.Base_Command):
 
     def transito(self):
         web = Web()
-        answer = web.html(web.get('http://cetsp1.cetsp.com.br/monitransmapa/agora/'))
-        if answer:
+        answerSP = web.html(web.get('http://cetsp1.cetsp.com.br/monitransmapa/agora/'))
+        if answerSP:
             dados = {
-                'hora': answer.find('div', id="hora").findAll(text=True)[0],
-                'lentidao': answer.find('div', id="lentidao").findAll(text=True)[0],
+                'hora': answerSP.find('div', id="hora").findAll(text=True)[0],
+                'lentidao': answerSP.find('div', id="lentidao").findAll(text=True)[0],
             }
             result = "%(lentidao)s km de transito em SP, atualizado as %(hora)s" % dados
 	    self.parent.conn.privmsg(self.channel, result)
-	    self.parent.conn.privmsg(self.channel, '%s' % ('Quer o transito de BH? Procura na porra da bhtrans pra ver se tem alguma coisa naquela bosta.'))
-        else:
-            return False
-	answer = web.html(web.get('http://www.maplink.com.br/MG/belo_horizonte/TransitoAgora'))
-        if answer:
-            dados = {
-                'hora': answer.find('div', id="totalSlow").findAll(text=True)[0],
-                'lentidao': answer.find('div', id="totalSlow").findAll(text=True)[0],
-            }
-	    lentidao = lentidao.split(' ')
-	    lentidao = lentidao[8]+lentidao[9]
-            result = "%(lentidao)s km de transito em SP, atualizado as %(hora)s" % dados
-	    self.parent.conn.privmsg(self.channel, result)
-            self.parent.conn.privmsg(self.channel, '%s' % ('Quer o transito de BH? Procura na porra da bhtrans pra ver se tem alguma coisa naquela bosta.'))
-
         else:
             return False
 
