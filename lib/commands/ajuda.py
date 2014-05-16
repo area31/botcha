d844ec5a95ba5b3eed2516a233b65bcbfc4d8c4e
coffnix <raphaebastos@hackstore.com.br>
2014-05-16 11:53:00 -0300

    new file:   lib/commands/ajuda.py

---
+++ lib/commands/ajuda.py
@@ -0,0 +1,46 @@
+#  -*- coding: utf-8 -*-
+#
+from lib.commands import Base_Command
+
+class Ajuda(Base_Command.Base_Command):
+
+    def ajuda(self):
+	if len(self.args) < 1:
+		self.parent.conn.privmsg(self.channel, '%s' % ('-- comandos disponiveis --'))
+		self.parent.conn.privmsg(self.channel, '%s' % ('!ajuda  |  !add  |  !chuck  |  !goatrance  |  !mulher  |  !quote  |  !transito  |  !docs  |  !zuera  |  !xinga'))
+	else:
+			if self.args[0] == 'ajuda':
+				self.parent.conn.privmsg(self.channel, '%s' % ('!ajuda nao possui parametros'))				
+			if self.args[0] == 'chuck':
+				self.parent.conn.privmsg(self.channel, '%s' % ('!chuck nao possui parametros'))
+			if self.args[0] == 'goatrance':
+				self.parent.conn.privmsg(self.channel, '%s' % ('!goatrance nao possui parametros'))
+			if self.args[0] == 'mulher':
+				self.parent.conn.privmsg(self.channel, '%s' % ('!mulher nao possui parametros'))
+			if self.args[0] == 'quote':
+				self.parent.conn.privmsg(self.channel, '%s' % ('!quote nao possui parametros'))
+			if self.args[0] == 'transito':
+				self.parent.conn.privmsg(self.channel, '%s' % ('!transito nao possui parametros'))
+			if self.args[0] == 'xinga':
+				self.parent.conn.privmsg(self.channel, '%s' % ('!xinga nickname'))
+			if self.args[0] == 'add':
+				if len(self.args) > 1:
+					if self.args[1] == 'goatrance':
+						self.parent.conn.privmsg(self.channel, '%s' % ('!add goatrance url'))
+					if self.args[1] == 'mulher':
+						self.parent.conn.privmsg(self.channel, '%s' % ('!add mulher link da mulher'))
+					if self.args[1] == 'quote':
+						self.parent.conn.privmsg(self.channel, '%s' % ('!add quote frase'))
+					if self.args[1] == 'xinga':
+						self.parent.conn.privmsg(self.channel, '%s' % ('!add xinga frase'))
+				else:
+					self.parent.conn.privmsg(self.channel, '%s' % ('-- ajudas disponiveis --'))
+					self.parent.conn.privmsg(self.channel, '%s' % ('!ajuda add goatrance'))
+					self.parent.conn.privmsg(self.channel, '%s' % ('!ajuda add mulher'))
+					self.parent.conn.privmsg(self.channel, '%s' % ('!ajuda add quote'))
+					self.parent.conn.privmsg(self.channel, '%s' % ('!ajuda add xinga'))
+		
+
+
+    def run(self):
+        self.ajuda()
