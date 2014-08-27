d47e4d8b7a8c5ea3458a944bdedb43680665ec34
coffnix <raphaebastos@hackstore.com.br>
2014-08-27 17:46:21 -0300

    modified:   lib/commands/crypto.py 	deleted:    random_guei.sh

--- lib/commands/crypto.py
+++ lib/commands/crypto.py
@@ -12,7 +12,7 @@ class Crypto(Base_Command.Base_Command):
                 _hash = db.select('hash', 'crypto')[0][0]
 	        self.parent.conn.privmsg(self.nick, '######################################################################')
 	        self.parent.conn.privmsg(self.nick, 'Remova a senha antiga:    /delkey #area31')
-	        self.parent.conn.privmsg(self.nick, 'Adicione a nova senha:    /setkey ' + _hash)
+	        self.parent.conn.privmsg(self.nick, 'Adicione a nova senha (de dentro do #area31):    /setkey ' + _hash)
                 self.parent.conn.privmsg(self.channel,'%s, Te enviei a senha do dia via PVT. Dicas, digite: !ajuda crypto     [ Qualquer dúvida RTFM --> http://a31.com.br/crypto-irc ]' % self.nick)
             except:
                 return False
