54fb4a7c95fb1306008b652b9639e04c6d75933b
coffnix <raphaebastos@hackstore.com.br>
2014-10-21 15:55:31 -0200

    fix for bug: UnicodeDecodeError: 'utf8' codec can't decode byte 0xe3 in position 1: invalid continuation byte

--- lib/commands/xinga.py
+++ lib/commands/xinga.py
@@ -7,16 +7,16 @@ class Xinga(Base_Command.Base_Command):
 
     def xinga(self):
         if len(self.args) < 1:
-            self.parent.conn.privmsg(self.channel,'%s, deixa de ser burro e xinga alguém. ' % self.nick)	
+            self.parent.conn.privmsg(self.channel,'%s, deixa de ser burro e xinga alguém direito. ' % self.nick)	
         else:
             db = Database()
             self.xinga = None
             try:
                 self.xinga = db.select('xinga', 'xingamentos', 1)[0][0]
+                if self.xinga:
+                    self.parent.conn.privmsg(self.channel, '%s, %s' % (self.args[0].decode('utf-8').encode('utf-8'), self.xinga))
             except:
                 return False
-            if self.xinga:
-                self.parent.conn.privmsg(self.channel, '%s, %s' % (self.args[0], self.xinga))
 
     def run(self):
         self.xinga()
